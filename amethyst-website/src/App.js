318e28f8ab74b20fa67d7d0a16664ff90d07ff89
Vedant-Tejaswi <tejaswi.vedant@gmail.com>
2025-06-05 22:48:51 +0530

    react

---
+++ amethyst-website/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+function App (){
+  return(
+    <div>
+      <h1 className='head'>Amethyst</h1>
+      <h3 className='bottom'>Integrated No Pollution Catalytic Converter</h3>
+    </div>
+  )
+}
+
+export default App
